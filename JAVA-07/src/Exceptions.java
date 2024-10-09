a5b3ce3b1791976f6dbee8da8e385d708ef6428a
Loch <42510283+Atom112@users.noreply.github.com>
2024-10-09 12:53:22 +0800

    Add files via upload

---
+++ JAVA-07/src/Exceptions.java
@@ -0,0 +1,11 @@
+class EmptyFileException extends Exception {
+    public EmptyFileException(String message) {
+        super(message);
+    }
+}
+
+class FileNotFoundException2 extends Exception {
+    public FileNotFoundException2(String message) {
+        super(message);
+    }
+}
